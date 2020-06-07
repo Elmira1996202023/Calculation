6beb28fbdeacd21eee1eed36c2402b6b7f373029
elmira1996 <elmira.kechalova@mail.ru>
2020-06-07 22:33:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Enter readVariables = new Enter();
+        Calculation resultCalculation = new Calculation();
+        readVariables.readForConsole();
+        System.out.println("Результат выполнения : " + resultCalculation.calculation(readVariables.isRome,readVariables.operation, readVariables.number1, readVariables.number2));
+    }
+}
